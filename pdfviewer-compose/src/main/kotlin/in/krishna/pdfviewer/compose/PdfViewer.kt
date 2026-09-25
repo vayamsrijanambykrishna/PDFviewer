032c31afbf22050179f3117cc910802fafe20363
vayamsrijanambykrishna <vayamsrijanambykrishna@gmail.com>
2026-09-25 18:31:24 +0530

    V7: add public API, Compose adapter, documentation and consumer rules

---
+++ pdfviewer-compose/src/main/kotlin/in/krishna/pdfviewer/compose/PdfViewer.kt
@@ -0,0 +1,43 @@
+package `in`.krishna.pdfviewer.compose
+
+import android.net.Uri
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import `in`.krishna.pdfviewer.PdfView
+import `in`.krishna.pdfviewer.PdfViewerState
+
+@Composable
+fun PdfViewer(
+    uri: Uri,
+    modifier: Modifier = Modifier,
+    state: PdfViewerState = remember { PdfViewerState.create() }
+) {
+    AndroidView(
+        modifier = modifier.fillMaxSize(),
+        factory = { context ->
+            PdfView(context).also { view ->
+                view.setDocument(uri)
+                view.setOnPageChangedListener { page ->
+                    state.currentPage = page
+                    state.pageCount = view.pageCount
+                    state.zoom = view.zoom
+                }
+            }
+        },
+        update = { view ->
+            state.currentPage = view.currentPage
+            state.pageCount = view.pageCount
+            state.zoom = view.zoom
+        }
+    )
+
+    DisposableEffect(uri) {
+        onDispose { }
+    }
+}
+
+private fun PdfViewerState.Companion.create(): PdfViewerState = PdfViewerState()
