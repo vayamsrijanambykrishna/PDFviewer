65f81c909fb14268bad7f341bce1b3f5809a0c49
vayamsrijanambykrishna <vayamsrijanambykrishna@gmail.com>
2026-09-25 18:17:18 +0530

    Add V1 PDF viewer: build.gradle.kts

---
+++ pdfviewer/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "in.krishna.pdfviewer"
+
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 28
+    }
+}
