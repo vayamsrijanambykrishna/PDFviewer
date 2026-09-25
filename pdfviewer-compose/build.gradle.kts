851804051c7eb422b98867f5eb66b2b5414947dd
vayamsrijanambykrishna <vayamsrijanambykrishna@gmail.com>
2026-09-25 18:36:27 +0530

    V8: publish the Compose adapter with release metadata and optional signing

--- pdfviewer-compose/build.gradle.kts
+++ pdfviewer-compose/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+    id("maven-publish")
+    id("signing")
 }
 
 android {
@@ -10,9 +13,84 @@ android {
     defaultConfig {
         minSdk = 28
     }
+
+    buildFeatures {
+        compose = true
+    }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
 }
 
 dependencies {
     implementation(project(":pdfviewer"))
-    implementation("androidx.compose.ui:ui")
+    implementation(libs.compose.ui)
+}
+
+val versionName = project.version.toString()
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = project.group.toString()
+            artifactId = "pdfviewer-compose"
+            version = versionName
+
+            afterEvaluate {
+                from(components["release"])
+            }
+
+            pom {
+                name.set("PDFviewer Compose")
+                description.set("Jetpack Compose adapter for the lightweight PDFviewer Android library.")
+                url.set("https://github.com/vayamsrijanambykrishna/PDFviewer")
+                licenses {
+                    license {
+                        name.set("Apache License 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("vayamsrijanambykrishna")
+                        name.set("Vayam Srijanam by Krishna")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/vayamsrijanambykrishna/PDFviewer.git")
+                    developerConnection.set("scm:git:ssh://git@github.com/vayamsrijanambykrishna/PDFviewer.git")
+                    url.set("https://github.com/vayamsrijanambykrishna/PDFviewer")
+                }
+            }
+        }
+    }
+}
+
+val repositoryUrl = System.getenv("MAVEN_REPOSITORY_URL")
+if (!repositoryUrl.isNullOrBlank()) {
+    publishing {
+        repositories {
+            maven {
+                name = "Remote"
+                url = uri(repositoryUrl)
+                credentials {
+                    username = System.getenv("MAVEN_USERNAME")
+                    password = System.getenv("MAVEN_PASSWORD")
+                }
+            }
+        }
+    }
+}
+
+val signingKey = System.getenv("SIGNING_KEY")
+val signingPassword = System.getenv("SIGNING_PASSWORD")
+if (!signingKey.isNullOrBlank() && !signingPassword.isNullOrBlank()) {
+    signing {
+        useInMemoryPgpKeys(signingKey, signingPassword)
+        sign(publishing.publications["release"])
+    }
 }
