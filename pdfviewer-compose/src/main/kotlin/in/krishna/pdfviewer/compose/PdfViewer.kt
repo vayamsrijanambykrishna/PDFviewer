095d9fdf5224931f6ef74d2233270af641812e4c
vayamsrijanambykrishna <vayamsrijanambykrishna@gmail.com>
2026-09-25 18:35:32 +0530

    V8: make Compose viewer lifecycle and URI updates robust

--- pdfviewer-compose/src/main/kotlin/in/krishna/pdfviewer/compose/PdfViewer.kt
+++ pdfviewer-compose/src/main/kotlin/in/krishna/pdfviewer/compose/PdfViewer.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import `in`.krishna.pdfviewer.PdfView
 import `in`.krishna.pdfviewer.PdfViewerState
@@ -16,18 +17,12 @@ fun PdfViewer(
     modifier: Modifier = Modifier,
     state: PdfViewerState = remember { PdfViewerState() }
 ) {
+    val context = LocalContext.current
+    val pdfView = remember { PdfView(context) }
+
     AndroidView(
         modifier = modifier.fillMaxSize(),
-        factory = { context ->
-            PdfView(context).also { view ->
-                view.setDocument(uri)
-                view.setOnPageChangedListener { page ->
-                    state.currentPage = page
-                    state.pageCount = view.pageCount
-                    state.zoom = view.zoom
-                }
-            }
-        },
+        factory = { pdfView },
         update = { view ->
             state.currentPage = view.currentPage
             state.pageCount = view.pageCount
@@ -35,7 +30,20 @@ fun PdfViewer(
         }
     )
 
-    DisposableEffect(uri) {
+    DisposableEffect(pdfView) {
+        pdfView.setOnPageChangedListener { page ->
+            state.currentPage = page
+            state.pageCount = pdfView.pageCount
+            state.zoom = pdfView.zoom
+        }
+        onDispose {
+            pdfView.setOnPageChangedListener(null)
+            pdfView.closeDocument()
+        }
+    }
+
+    DisposableEffect(uri, pdfView) {
+        pdfView.setDocument(uri)
         onDispose { }
     }
 }
