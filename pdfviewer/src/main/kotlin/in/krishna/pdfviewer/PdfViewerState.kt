e1e6f4dfc9e1300f0586a325988a9aa8228e8e85
vayamsrijanambykrishna <vayamsrijanambykrishna@gmail.com>
2026-09-25 18:35:11 +0530

    V8: make viewer state usable from the separate Compose artifact

--- pdfviewer/src/main/kotlin/in/krishna/pdfviewer/PdfViewerState.kt
+++ pdfviewer/src/main/kotlin/in/krishna/pdfviewer/PdfViewerState.kt
@@ -2,11 +2,6 @@ package `in`.krishna.pdfviewer
 
 class PdfViewerState {
     var currentPage: Int = -1
-        internal set
-
     var pageCount: Int = 0
-        internal set
-
     var zoom: Float = 1f
-        internal set
 }
