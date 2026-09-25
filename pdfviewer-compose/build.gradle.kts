9de84490e65a53c734a1d3b0ea657ae662bf03eb
vayamsrijanambykrishna <vayamsrijanambykrishna@gmail.com>
2026-09-25 18:44:31 +0530

    V8.1: use AGP 9 built-in Kotlin support in Compose module

--- pdfviewer-compose/build.gradle.kts
+++ pdfviewer-compose/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-    id("org.jetbrains.kotlin.plugin.compose")
+        id("org.jetbrains.kotlin.plugin.compose")
     id("maven-publish")
     id("signing")
 }
